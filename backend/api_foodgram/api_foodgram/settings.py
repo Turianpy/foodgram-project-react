c6c6bb932dc3dba346a4fe0d0c925a6c2c98d6af
turianpy <turianpy@gmail.com>
2023-04-29 04:06:43 +0300

    deploy workflow optimizations

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -1,6 +1,9 @@
 import datetime
+import mimetypes
 import os
 from pathlib import Path
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_backend')
 
 MEDIA_URL = '/media/'
